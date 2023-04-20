41a55aab9569938ed13607def07e917330dc81e1
Damirbey <dydamirbey@gmail.com>
2023-04-20 13:50:43 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <a href="#">amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
