601b5a2cde938e14ddc9aa29a6ff0b763c79d7c3
Damirbey <dydamirbey@gmail.com>
2023-05-19 10:24:57 -0400

    publishing web app

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRouter.js';
@@ -37,6 +37,13 @@ app.use('/api/orders', orderRouter);
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+//SERVING APP ON THE SERVER
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
+
 //ERROR HANDLING API
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
