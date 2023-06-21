24bdcc01284c9dd79ec0132b4d12a0096a84687c
Damirbey <dydamirbey@gmail.com>
2023-06-21 19:55:11 -0400

    Deployment

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -29,7 +29,7 @@ function HomeScreen() {
     const fetchProducts = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get('/api/products');
+        const result = await axios.get('https://damir-app.onrender.com/api/products');
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
