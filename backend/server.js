5a524f607cc31c0e07b3e2c6e83f47e1697895af
Damirbey <dydamirbey@gmail.com>
2023-06-22 10:35:53 -0400

    Modified server settings

--- backend/server.js
+++ backend/server.js
@@ -55,23 +55,12 @@ app.get('*', (req, res) => {
 });
 
 //ERROR HANDLING API
-/*app.use((err, req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use((err, req, res, next) => {
+  /*res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
+    next();*/
   res.status(500).send({ message: err.message });
-});*/
-app.use((req, res, next) => {
-  //allow access from every, elminate CORS
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.removeHeader('x-powered-by');
-  //set the allowed HTTP methods to be requested
-  res.setHeader('Access-Control-Allow-Methods','POST');
-  //headers clients can use in their requests
-  res.setHeader('Access-Control-Allow-Headers','Content-Type');
-  //allow request to continue and be handled by routes
-  next();
 });
 
 //RUNNING THE SERVER
